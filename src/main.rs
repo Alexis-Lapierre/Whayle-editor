127436494c81953dc77986d23e44d064127e1125
Alexis Lapierre <128792625+Alexis-Lapierre@users.noreply.github.com>
2025-01-24 10:35:01 +0100

    Fix first pokemon being empty

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-const END_OF_POKEMON: [u8; 4] = [0xff, 0xff, 0xff, 0xff];
+const DELIMITER_LEN: usize = 4;
+const POKEMON_DELIMITER: [u8; DELIMITER_LEN] = [0xff, 0xff, 0xff, 0xff];
 
 fn find_subsequence<T>(haystack: &[T], needle: &[T]) -> Option<usize>
 where
@@ -12,9 +13,9 @@ where
 fn list_pokemons(bytes: &[u8]) -> impl Iterator<Item = Vec<Move>> + '_ {
     let mut current_remaining_data = bytes;
     std::iter::from_fn(move || {
-        find_subsequence(current_remaining_data, &END_OF_POKEMON).map(|position| {
+        find_subsequence(current_remaining_data, &POKEMON_DELIMITER).map(|position| {
             let (pokemon_data, remaining) = current_remaining_data.split_at(position);
-            current_remaining_data = &remaining[4..];
+            current_remaining_data = &remaining[DELIMITER_LEN..];
             pokemon_data
                 .chunks(4)
                 .map(|chunk| Move::from(&chunk.try_into().unwrap()))
@@ -41,9 +42,9 @@ impl From<&[u8; 4]> for Move {
 fn main() {
     let bytes = std::fs::read("./a018.narc").expect("Should have been able to read the file");
     let poke_bytes = {
-        let ignored = find_subsequence(&bytes, &END_OF_POKEMON)
+        let ignored = find_subsequence(&bytes, &POKEMON_DELIMITER)
             .expect("At least one pokemon delimited in file");
-        &bytes[ignored..]
+        &bytes[(ignored + DELIMITER_LEN)..]
     };
 
     for (id, pokemon) in list_pokemons(poke_bytes).enumerate() {
