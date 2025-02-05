672031da076938906fda2adc4e21e6bf8965cbbe
Alexis Lapierre <128792625+Alexis-Lapierre@users.noreply.github.com>
2025-02-05 22:52:07 +0100

    Application can now delete move

--- src/tui/app.rs
+++ src/tui/app.rs
@@ -1,5 +1,3 @@
-use std::any::Any;
-
 use crate::{
     file::{Move, SaveFile},
     names::POKE_NAMES,
@@ -12,12 +10,17 @@ use ratatui::{
 };
 
 pub struct App {
-    current_pokemon: usize,
     save_file: SaveFile,
-    selection: Selection,
+    gui_state: State,
 }
 
-enum Selection {
+struct State {
+    current_pokemon: usize,
+    selected: Selected,
+}
+
+#[derive(PartialEq, Eq)]
+enum Selected {
     Pokemon,
     Move(usize),
 }
@@ -26,21 +29,39 @@ impl App {
     pub fn from_save_file(save_file: SaveFile) -> Self {
         debug_assert!(save_file.pokemons.len() == POKE_NAMES.len());
         Self {
-            current_pokemon: 0,
-            selection: Selection::Pokemon,
+            gui_state: State {
+                current_pokemon: 0,
+                selected: Selected::Pokemon,
+            },
             save_file,
         }
     }
-    pub const fn next_pokemon(&mut self) {
-        self.current_pokemon += 1;
-        if self.current_pokemon >= POKE_NAMES.len() {
-            self.current_pokemon = 0;
-        }
+
+    pub fn select_next(&mut self) {
+        self.gui_state.next(self.get_moves().len());
     }
-    pub const fn previous_pokemon(&mut self) {
-        self.current_pokemon = match self.current_pokemon.checked_sub(1) {
-            Some(value) => value,
-            None => POKE_NAMES.len() - 1,
+
+    pub fn select_previous(&mut self) {
+        self.gui_state.previous(self.get_moves().len());
+    }
+    pub fn select_right(&mut self) {
+        self.gui_state.select_moves();
+    }
+    pub fn select_left(&mut self) {
+        self.gui_state.select_pokemon();
+    }
+
+    pub fn delete_move_selected(&mut self) {
+        match self.gui_state.selected {
+            Selected::Pokemon => todo!("Handle delete when no move is selected, AKA do nothing."),
+            Selected::Move(index) => {
+                self.get_moves_mut().remove(index);
+                self.gui_state.selected = match self.get_moves().len() {
+                    0 => Selected::Pokemon, // todo handle empty move set...
+                    n if index >= n => Selected::Move(n - 1),
+                    _ => Selected::Move(index),
+                }
+            }
         }
     }
 
@@ -61,16 +82,21 @@ impl App {
             .zip(POKE_NAMES)
             .map(|(id, name)| Row::new([id.to_string(), name.to_string()]));
         let widths = [Constraint::Length(5), Constraint::Length(25)];
-        Table::new(rows, widths)
+        let table = Table::new(rows, widths)
             .header(Row::new(["ID", "Name"]))
             .block(Block::bordered().title("Pokemons"))
             .row_highlight_style(Style::new().reversed())
-            .highlight_spacing(HighlightSpacing::Always)
-            .highlight_symbol(">>")
+            .highlight_spacing(HighlightSpacing::Always);
+
+        if self.gui_state.selected == Selected::Pokemon {
+            table.highlight_symbol(">>")
+        } else {
+            table
+        }
     }
 
     fn pokemon_state(&self) -> TableState {
-        TableState::default().with_selected(self.current_pokemon)
+        TableState::default().with_selected(self.gui_state.current_pokemon)
     }
 
     fn move_table(&self) -> Table<'_> {
@@ -86,22 +112,83 @@ impl App {
             Constraint::Length(20),
             Constraint::Length(5),
         ];
-        Table::new(moves, widths)
+        let table = Table::new(moves, widths)
             .header(Row::new(["ID", "Name", "Level"]))
             .block(Block::bordered().title("Moves"))
             .row_highlight_style(Style::new().reversed())
-            .highlight_spacing(HighlightSpacing::Always)
+            .highlight_spacing(HighlightSpacing::Always);
+
+        if let Selected::Move(_) = self.gui_state.selected {
+            table.highlight_symbol(">>")
+        } else {
+            table
+        }
     }
 
     fn move_state(&self) -> TableState {
-        match self.selection {
-            Selection::Move(index) => TableState::default().with_selected(index),
-            Selection::Pokemon => TableState::default(),
+        match self.gui_state.selected {
+            Selected::Move(index) => {
+                debug_assert!(index < self.get_moves().len());
+                TableState::default().with_selected(index)
+            }
+            Selected::Pokemon => TableState::default(),
         }
     }
 
     fn get_moves(&self) -> &[Move] {
-        debug_assert!(self.current_pokemon < self.save_file.pokemons.len());
-        &self.save_file.pokemons[self.current_pokemon]
+        debug_assert!(self.gui_state.current_pokemon < self.save_file.pokemons.len());
+        &self.save_file.pokemons[self.gui_state.current_pokemon]
+    }
+
+    fn get_moves_mut(&mut self) -> &mut Vec<Move> {
+        debug_assert!(self.gui_state.current_pokemon < self.save_file.pokemons.len());
+        &mut self.save_file.pokemons[self.gui_state.current_pokemon]
+    }
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            current_pokemon: 0,
+            selected: Selected::Pokemon,
+        }
+    }
+}
+
+impl State {
+    const fn next(&mut self, max_move_len: usize) {
+        if let Selected::Move(index) = self.selected {
+            let index = index + 1;
+            self.selected = Selected::Move(if index >= max_move_len { 0 } else { index })
+        } else {
+            self.next_pokemon();
+        }
+    }
+    fn previous(&mut self, max_move_len: usize) {
+        if let Selected::Move(index) = self.selected {
+            self.selected = Selected::Move(index.checked_sub(1).unwrap_or(max_move_len - 1));
+        } else {
+            self.previous_pokemon();
+        }
+    }
+    const fn next_pokemon(&mut self) {
+        self.current_pokemon += 1;
+        if self.current_pokemon >= POKE_NAMES.len() {
+            self.current_pokemon = 0;
+        }
+    }
+
+    const fn previous_pokemon(&mut self) {
+        self.current_pokemon = match self.current_pokemon.checked_sub(1) {
+            Some(value) => value,
+            None => POKE_NAMES.len() - 1,
+        }
+    }
+    const fn select_pokemon(&mut self) {
+        self.selected = Selected::Pokemon;
+    }
+
+    const fn select_moves(&mut self) {
+        self.selected = Selected::Move(0);
     }
 }
--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -8,19 +8,18 @@ use crate::file::SaveFile;
 
 mod app;
 
-pub fn run(mut terminal: DefaultTerminal, mut save_file: SaveFile) {
+pub fn run(mut terminal: DefaultTerminal, save_file: SaveFile) {
     let mut app = App::from_save_file(save_file);
     loop {
         terminal.draw(|frame| app.render(frame)).unwrap();
         match event::read().unwrap() {
             Event::Key(key) if key.kind == KeyEventKind::Press => match key.code {
                 KeyCode::Char('q') | KeyCode::Esc => break,
-                KeyCode::Up => {
-                    app.previous_pokemon();
-                }
-                KeyCode::Down => {
-                    app.next_pokemon();
-                }
+                KeyCode::Up => app.select_previous(),
+                KeyCode::Down => app.select_next(),
+                KeyCode::Right => app.select_right(),
+                KeyCode::Left => app.select_left(),
+                KeyCode::Char('d') => app.delete_move_selected(),
                 _ => {}
             },
             _ => {}
